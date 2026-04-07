4318f1b3fbfc8adef41bca24cc422dadc2d3c67c
U <afsau@deloitte.com>
2026-04-07 15:57:16 +0530

    config file changes

--- playwright.config.js
+++ playwright.config.js
@@ -35,7 +35,7 @@ export default defineConfig({
       use: {
         /* Base URL to use in actions like `await page.goto('')`. */
         baseURL: 'https://www.saucedemo.com',
-        headless: false,
+        headless: true,
         screenshot: 'only-on-failure',
         video: 'retain-on-failure',
         trace: 'on-first-retry',
