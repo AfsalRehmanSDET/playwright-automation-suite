6a1d38224ac6bb3dcb88d1210260f6ab6cf88970
U <afsau@deloitte.com>
2026-04-07 13:25:33 +0530

    page route added for UI TestCase

--- tests/ui/cart.spec.ts
+++ tests/ui/cart.spec.ts
@@ -1,16 +1,31 @@
-import {test, expect} from '@playwright/test';
+import { test, expect } from '../../fixtures';
 import { LoginPage } from '../../pages/LoginPage';
 import { InventoryPage } from '../../pages/InventoryPage';
+import { TAGS } from '../../constants';
 
-test('Add an item to the cart', async ({page}) => {
-  // Navigate to the login page
-  const loginPage = new LoginPage(page);
-  const inventoryPage = new InventoryPage(page);
-  await loginPage.navigate();
-  await loginPage.login({ username: 'standard_user', password: 'secret_sauce' });
-  await expect(page).toHaveURL('/inventory.html');
+test(`Add an item to the cart`, async ({ loggedInPage }) => {
+  const title = await loggedInPage.getTitle();
+  await expect(loggedInPage.page).toHaveURL('/inventory.html');
+  const inventoryPage = new InventoryPage(loggedInPage.page);
   await inventoryPage.addItemToCart();
   await inventoryPage.goToCart();
 });
+
+test(`Show error when inventory API fails @route ${TAGS.UI}`, async ({ loggedInPage }) => {
+
+  await loggedInPage.page.route('**/inventory*', route => {
+    route.fulfill({
+      status: 500,
+      contentType: 'application/json',
+      body: JSON.stringify({ error: 'Internal Server Error' }),
+    });
+  });
+  await loggedInPage.page.pause(); // Wait for the route to be set up
+  await loggedInPage.page.reload();
+  const isInventoryVisible = await loggedInPage.isElementVisible(loggedInPage.page.locator('.inventory_list'));
+  expect(isInventoryVisible).toBe(false);
+});
+
+
 
 
